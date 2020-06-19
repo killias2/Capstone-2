c86978e7c85b417011a2e5edd458c0b1f8eb0b7b
Caroline Eberhart <caroeberhart@gmail.com>
2020-06-19 11:30:18 -0400

    created a few packages, menu classes

--- src/main/java/com/techelevator/CampgroundCLI.java
+++ src/main/java/com/techelevator/CampgroundCLI.java
@@ -5,6 +5,14 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class CampgroundCLI {
+	
+	private Menu menu;
+	private ParkDAO parkDAO;
+	private CampgroundDAO campgroundDAO;
+	private SiteDAO siteDAO;
+	private ReservationDAO reservationDAO;
+	
+	
 
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
@@ -18,9 +26,26 @@ public class CampgroundCLI {
 
 	public CampgroundCLI(DataSource datasource) {
 		// create your DAOs here
+		this.menu = new Menu();
+		parkDAO = new JDBCParkDAO(datasource);
+		campgroundDAO = new JDBC
+		
 	}
 
 	public void run() {
-
+		//call method to display banner
+		
+		while (true) {	//main logic loop for menu; calls other methods
+			//display parks page
+//			if (//userinput == any valid park number) {
+//				//run the method below that corresponds
+//			} else if (//userinput == Q) {
+//				//stop application from running
+//			}
+		}
 	}
+	
+	
+	
+	
 }
---
+++ src/main/java/com/techelevator/projects/view/Menu.java
@@ -0,0 +1,14 @@
+package com.techelevator.projects.view;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Menu {
+
+	//holds printwriter and scanner logic
+	//does the try/catch loops to check user input
+	
+	private PrintWriter out;
+	private Scanner in;
+	
+}
